cf9b8fb34870e1da3d5e68de1d9ae151d6698c51
Alex <gurievalex78@gmail.com>
2020-07-15 17:39:33 +0200

    Palindroma

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Palidroma
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per capire se la parola
+// inserita è palindroma
+
+ var parola = prompt('Inserisci una parola:');
+ var stampa = palindroma(parola);
+
+
+document.getElementById('stamp').innerHTML= stampa;
+
+//function
+function palindroma(string){
+  var ris ="";
+  var numeri = RegExp('[0-9]' );
+  var carSpec = RegExp('[!@#$%^&*()+=,./{}|:<>?]');
+  string = string.toUpperCase();
+  for (var i =  string.length-1; i >= 0; i--) {
+    ris += string[i];
+  }
+  if((numeri.test(string) == true) || (carSpec.test(string) == true) || (string == '') ||(string.length <2)){
+    return 'Questa volta ti perdono';
+  } else if(ris == string){
+    return "E' una parola palindroma";
+  } else {
+    return "Non è una parola palindroma";
+  }
+}
